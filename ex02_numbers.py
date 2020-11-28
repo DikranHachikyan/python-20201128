d34a75b2135e602a77f7b64c04b1cfd3182d8bed
Dikran H <dhachikian@expert-bg.org>
2020-11-28 18:10:33 +0200

    Python L01

---
+++ ex02_numbers.py
@@ -0,0 +1,20 @@
+# %%
+a = 10
+a
+# %%
+age = 35
+age
+# %%
+id(age)
+
+type(age)
+
+# %%
+id(age)
+# %%
+age = 25
+id(age)
+# %%
+size = 25
+id(size)
+# %%
---
+++ ex04_stings.py
@@ -0,0 +1,45 @@
+# %%
+s1 = 'Lorem ipsum dolor sit amet'
+s1
+
+# %%
+s2 = "Lorem ipsum dolor sit amet"
+s2
+# %%
+s3 = '''Lorem ipsum dolor sit amet,
+consectetur adipisicing elit, 
+sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'''
+s3
+# %%
+def foo():
+    '''Function foo
+    created: 28.11.2020
+    modified: 28.11.2020'''
+    print('Hello Python')
+#%%
+print(foo.__doc__)
+print(sorted.__doc__)
+
+#%%
+x = 12
+
+print(f'value of x**2 is {x**2}')
+
+#%%
+s1[2]
+
+#%%
+s1[0:5]
+
+#%%
+s1[0:20:2]
+#%%
+s1[::2]
+#%%
+s1[::-1]
+#%%
+# s1[0:20:2] = 'x'
+
+
+
+
